4e89229023dc646956a45a86bd0a156f3af64204
shad0www <ccff33brj@gmail.com>
2025-10-30 10:18:55 +0100

    Refactor guessing game: tidy imports, restructure main loop and input handling, remove debug print

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
 //https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
 
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your guess.");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+        let mut guess: String = String::new();
 
-    println!("Please input your guess.");
-    println!("The secret number is: {secret_number}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let mut guess = String::new();
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too smalll"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
+    }
 }
